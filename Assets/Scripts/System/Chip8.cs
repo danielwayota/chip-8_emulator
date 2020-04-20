4de74c88ade80e16eb4e2d7b9a1326e7e30af3cd
Daniel Wayota <danielglez1990@gmail.com>
2020-04-20 12:21:42 +0100

    Character data and op code decode

--- Assets/Scripts/System/Chip8.cs
+++ Assets/Scripts/System/Chip8.cs
@@ -1,3 +1,9 @@
+using UnityEngine;
+
+/// <summary>
+/// Documentation:
+/// http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+/// </summary>
 public class Chip8
 {
     // Registers
@@ -18,6 +24,8 @@ public class Chip8
     // RAM
     public byte[] ram  { protected set; get; }
 
+    public IRenderer screen;
+
     public const short PROGRAM_START = 0x200;
 
     /// =============================================
@@ -35,6 +43,7 @@ public class Chip8
         // The RAM is 4KB long
         this.ram = new byte[4096];
 
+        this.LoadCharacterSprites();
         this.Restart();
     }
 
@@ -72,20 +81,113 @@ public class Chip8
     }
 
     /// =============================================
+    public void LoadCharacterSprites()
+    {
+        byte[] characterSprites = {
+            // 0
+            0xF0, 0x90, 0x90, 0x90, 0xF0,
+            // 1
+            0x20, 0x60, 0x20, 0x20, 0x70,
+            // 2
+            0xF0, 0x10, 0xF0, 0x80, 0xF0,
+            // 3
+            0xF0, 0x10, 0xF0, 0x10, 0xF0,
+            // 4
+            0x90, 0x90, 0xF0, 0x10, 0x10,
+            // 5
+            0xF0, 0x80, 0xF0, 0x10, 0xF0,
+            // 6
+            0xF0, 0x80, 0xF0, 0x90, 0xF0,
+            // 7
+            0xF0, 0x10, 0x20, 0x40, 0x40,
+            // 8
+            0xF0, 0x90, 0xF0, 0x90, 0xF0,
+            // 9
+            0xF0, 0x90, 0xF0, 0x10, 0xF0,
+            // A
+            0xF0, 0x90, 0xF0, 0x90, 0x90,
+            // B
+            0xE0, 0x90, 0xE0, 0x90, 0xE0,
+            // C
+            0xF0, 0x80, 0x80, 0x80, 0xF0,
+            // D
+            0xE0, 0x90, 0x90, 0x90, 0xE0,
+            // E
+            0xF0, 0x80, 0xF0, 0x80, 0xF0,
+            // F
+            0xF0, 0x80, 0xF0, 0x80, 0x80
+        };
+
+        var index = 0;
+
+        foreach (var data in characterSprites)
+        {
+            this.ram[index] = data;
+            index++;
+        }
+    }
+
+    /// =============================================
     /// <summary>
     /// Executes one machine cicle
     /// </summary>
     public void Tick()
     {
         // Fetch instruction
-        // Decode instruction
-        // Execute instruction
+        byte up = this.ram[this.programCounter];
+        byte down = this.ram[this.programCounter + 1];
+
+        short opCode = (short)(up << 8 | down);
+
+        this.ExecuteOpCode(opCode);
+
         // Advance program counter
         this.programCounter += 2;
 
         if (this.programCounter > this.ram.Length)
         {
             this.programCounter = PROGRAM_START;
+        }
+    }
+
+    /// =============================================
+    public void ExecuteOpCode(short opCode)
+    {
+        // Decode instruction
+
+        // Execute instruction
+
+        byte codeIndex = (byte)((opCode & 0xF000) >> 12);
+
+        switch (codeIndex)
+        {
+            case 0:
+                // 00E0 - CLS
+                // Clear the display.
+                if (opCode == 0x00E0)
+                {
+                    this.screen.Clear();
+                }
+
+
+                // 00EE - RET
+                // Return from a subroutine.
+
+                // The interpreter sets the program counter to the address at the top of the stack,
+                // then subtracts 1 from the stack pointer.
+                break;
+
+            // 6xkk - LD Vx, byte
+            // Set Vx = kk.
+            // The interpreter puts the value kk into register Vx.
+            case 6:
+                int x = (opCode & 0xF00) >> 8;
+                byte kk = (byte)(opCode & 0xFF);
+
+                this.v[x] = kk;
+
+                break;
+
         }
     }
 }
